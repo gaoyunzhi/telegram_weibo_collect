79dbf23323937aa1795be40cbef5a11e20ad3366
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-12 20:59:09 -0700

    commit

--- weibo_aggregator.py
+++ weibo_aggregator.py
@@ -14,6 +14,7 @@ from soup_get import SoupGet, Timer
 from db import DB
 import threading
 import weibo_2_album
+import urllib
 
 with open('credential') as f:
 	credential = yaml.load(f, Loader=yaml.FullLoader)
@@ -25,129 +26,6 @@ channel = tele.bot.get_chat(-1001374366482)
 
 sg = SoupGet()
 db = DB()
-
-# def dataCount(item):
-# 	for x in item.find_all('span', class_='count'):
-# 		r = int(x.get('data-count'))
-# 		if r:
-# 			yield r
-
-# def wantSee(item, page, channel_name):
-# 	if matchKey(str(item.parent), ['people/gyz', '4898454']):
-# 		return True
-# 	if matchKey(str(item), db.getBlacklist(channel_name)):
-# 		return False
-# 	require = 120 + page
-# 	if 'people/renjiananhuo' in str(item.parent):
-# 		require *= 4 # 这人太火，发什么都有人点赞。。。
-# 	return sum(dataCount(item)) > require
-
-# def getSource(item):
-# 	new_status = item
-# 	while 'new-status' not in new_status.get('class'):
-# 		new_status = new_status.parent
-# 	if new_status.get('data-sid'):
-# 		return 'https://www.douban.com/people/%s/status/%s/' % \
-# 			(new_status['data-uid'], new_status['data-sid'])
-
-# def getCap(quote, url):
-# 	if '_' in url:
-# 		url = '[%s](%s)' % (url, url)
-# 	return cutCaption(quote, url, 4000)
-
-# def getResult(post_link, item):
-# 	raw_quote = item.find('blockquote') or ''
-# 	quote = export_to_telegraph.exportAllInText(raw_quote)
-# 	quote = compactText(quote).strip('更多转发...').strip()
-
-# 	r = web_2_album.Result()
-
-# 	note = item.find('div', class_='note-block')
-# 	if (note and note.get('data-url')) or matchKey(post_link, 
-# 			['https://book.douban.com/review/', 'https://www.douban.com/note/']):
-# 		note = (note and note.get('data-url')) or post_link
-# 		url = export_to_telegraph.export(note, force=True) or note
-# 		r.cap = getCap(quote, url)
-# 		return r
-
-# 	if item.find('div', class_='url-block'):
-# 		url = item.find('div', class_='url-block')
-# 		url = url.find('a')['href']
-# 		url = clearUrl(export_to_telegraph.export(url) or url)
-# 		r.cap = getCap(quote, url)
-# 		return r
-
-# 	if '/status/' in post_link:
-# 		r = web_2_album.get(post_link, force_cache=True)
-# 		r.cap = quote
-# 		if r.imgs:
-# 			return r
-
-# 	print('useless additional fetch', post_link)
-
-# 	if quote and raw_quote.find('a', title=True, href=True):
-# 		r.cap = quote
-# 		return r
-
-# def postTele(douban_channel, item, timer):
-# 	if not item or not item.find('span', class_='created_at'):
-# 		if '仅自己可见' in str(item):
-# 			return # 被审核掉的广播
-# 		print('no created at')
-# 		print(item)
-# 		# see how often this happen...
-# 		return
-# 	post_link = item.find('span', class_='created_at').find('a')['href']
-# 	source = getSource(item) or post_link
-# 	source = source.strip()
-# 	post_link = post_link.strip()
-
-# 	if db.exist(douban_channel.username, source):
-# 		return 'existing'
-# 	if db.exist(douban_channel.username, post_link):
-# 		return 'repeated_share'
-
-# 	result = getResult(post_link, item)
-# 	if result:
-# 		timer.wait(len(result.imgs or [1]) * 10)
-# 		r = album_sender.send(douban_channel, source, result)
-# 		db.addToExisting(douban_channel.username, post_link)
-# 		db.addToExisting(douban_channel.username, source)
-# 		return 'sent'
-
-# @log_on_fail(debug_group)
-# def processChannel(name, url_prefix):
-# 	existing = 0
-# 	print('start processing %s' % name)
-# 	timer = Timer()
-
-# 	douban_channel = tele.bot.get_chat('@' + name)
-# 	if 'status' in url_prefix:
-# 		page_range = range(50, 0, -1)
-# 	else:
-# 		page_range = range(1, 100)
-# 	for page in page_range:
-# 		if 'test' in sys.argv:
-# 			print('page: %d' % page)
-# 		url = url_prefix + '?p=' + str(page)
-# 		items = list(sg.getSoup(url, db.getCookie(name))
-# 			.find_all('div', class_='status-item'))
-# 		if not items and 'status' not in url_prefix:
-# 			debug_group.send_message('Cookie expired for channel: %s' % name)
-# 			return
-# 		for item in items:
-# 			if not wantSee(item, page, name):
-# 				continue
-# 			r = postTele(douban_channel, item, timer)
-# 			if r == 'sent' and 'skip' in sys.argv:
-# 				return # testing mode, only send one message
-# 			if r == 'existing':
-# 				existing += 1
-# 			elif r == 'sent':
-# 				existing = 0
-# 		if (existing > 10 or page * existing > 200) and 'once' not in sys.argv:
-# 			break
-# 	print('channel %s finished by visiting %d page' % (name, page))
 
 def removeOldFiles(d):
 	try:
@@ -162,12 +40,24 @@ def getSingleCount(blog):
 	return int(blog['reposts_count']) + int(blog['comments_count']) + int(blog['attitudes_count'])
 
 def getCount(blog):
+	if not blog:
+		return 0
 	count = getSingleCount(blog)
 	if 'retweeted_status' in blog:
 		blog = blog['retweeted_status']
 		count += getSingleCount(blog) / 3
 	return count
 
+def process(url):
+	content = sg.getContent(url)
+	content = yaml.load(content, Loader=yaml.FullLoader)
+	for card in content['data']['cards']:
+		if getCount(card.get('mblog')) < 120:
+			continue
+		url = clearUrl(card['scheme'])
+		r = weibo_2_album.get(url)
+		r = album_sender.send(channel, url, r)
+		# add the url to existing
 
 @log_on_fail(debug_group)
 def loopImp():
@@ -175,44 +65,27 @@ def loopImp():
 	removeOldFiles('tmp_image')
 	sg.reset()
 	db.reload()
-	for user in db.users.items:
-		url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=%s&containerid=107603%s' \
-			% (user, user)
-		content = sg.getContent(url)
-		content = yaml.load(content, Loader=yaml.FullLoader)
-		for card in content['data']['cards']:
-			if getCount(card['mblog']) < 120:
-				continue
-			url = clearUrl(card['scheme'])
-			r = weibo_2_album.get(url)
-			r = album_sender.send(channel, url, r)
-			# add the url to existing
+	# for user in db.users.items:
+	# 	url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=%s&containerid=107603%s' \
+	# 		% (user, user)
+	# 	process(url)
+	for keyword in db.keywords.items:
+		content_id = urllib.request.pathname2url('100103type=1&q=' + keyword)
+		print(content_id)
+		url = 'https://m.weibo.cn/api/container/getIndex?containerid=%s&page_type=searchall' % content_id
+		process(url)
+
 
 def loop():
 	loopImp()
 	threading.Timer(60 * 60 * 2, loop).start() 
-
-# def commandInternal(msg):
-# 	command, text = splitCommand(msg.text)
-# 	if matchKey(command, ['/d_sc', 'set_cookie']):
-# 		return db.setCookie(msg.chat.username, text)
-# 	if matchKey(command, ['/d_ba', 'blacklist_ba']):
-# 		return db.blacklistAdd(msg.chat.username, text)
-# 	if matchKey(command, ['/d_br', 'blacklist_br']):
-# 		return db.blacklistRemove(msg.chat.username, text)
-# 	if matchKey(command, ['/d_bl', 'blacklist_list']):
-# 		return 'blacklist:\n' + '\n'.join(db.getBlacklist(msg.chat.username))
 
 @log_on_fail(debug_group)
 def command(update, context):
 	msg= update.channel_post
 	if not msg.text.startswith('/w'):
 		return
-	# r = commandInternal(msg)
-	# if not r:
-	# 	return
-	# autoDestroy(msg.reply_text(r), 0.1)
-	# msg.delete()
+	# TODO: add command to add user, add keyword, refer douban code if needed
 
 if 'once' not in sys.argv:
 	tele.dispatcher.add_handler(MessageHandler(
