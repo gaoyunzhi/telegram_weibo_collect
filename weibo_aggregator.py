d758bb807a5999d8034cff698a9d0cc795c99481
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-15 02:22:28 -0700

    commit

--- weibo_aggregator.py
+++ weibo_aggregator.py
@@ -71,16 +71,16 @@ def loopImp():
 	removeOldFiles('tmp_image')
 	sg.reset()
 	db.reload()
-	for user in db.users.items:
-		url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=%s&containerid=107603%s' \
-			% (user, user)
-		process(url)
-		print(user)
 	for keyword in db.keywords.items:
 		content_id = urllib.request.pathname2url('100103type=1&q=' + keyword)
 		url = 'https://m.weibo.cn/api/container/getIndex?containerid=%s&page_type=searchall' % content_id
 		process(url)
 		print(keyword)
+	for user in db.users.items:
+		url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=%s&containerid=107603%s' \
+			% (user, user)
+		process(url)
+		print(user)
 
 def loop():
 	loopImp()
